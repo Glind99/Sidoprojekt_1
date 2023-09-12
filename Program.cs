0e1c28731e9ff11a86fe98c70d643c2a9deb5f03
gusta <gusta@DESKTOP-CJNJV8M>
2023-09-12 15:29:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sidoprojekt_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] users = { "Gustav", "elsa", "anders" };
+            for (int i = 0; i < 1; i++)
+            {
+             Console.WriteLine(users[2]);
+            }
+
+            
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
